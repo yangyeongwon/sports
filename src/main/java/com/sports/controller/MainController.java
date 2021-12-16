8974c89092a1090479f59ce2b046246c5cba9878
yywon123 <yywon123@gmail.com>
2021-12-16 18:02:01 +0900

    20211216_yyw

--- src/main/java/com/sports/controller/MainController.java
+++ src/main/java/com/sports/controller/MainController.java
@@ -6,11 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/sports")
+@RequestMapping("/sports/main")
 public class MainController {
 	
-	@GetMapping(value = "main")
+	@GetMapping(value = {"", "/"})
 	public String mainView(Model model) {
 		return "sports/main";
+	}
+	
+	@GetMapping(value = "dialog")
+	public String dialogView(Model model) {
+		return "sports/fragments/login_dialog";
 	}
 }
